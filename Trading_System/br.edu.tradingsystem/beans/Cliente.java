1812d8873e01eaa67b71274c4121f63672f7ac97
Andre Ricardo <Andre Ricardo@10.50.20.139>
2014-08-22 16:16:23 -0300

    criado a classe cliente, com os atributos, clientes sem id será convencionado como valor maximo do integer.

--- Trading_System/br.edu.tradingsystem/beans/Cliente.java
+++ Trading_System/br.edu.tradingsystem/beans/Cliente.java
@@ -2,4 +2,70 @@ package beans;
 
 public class Cliente {
 
+	private String nome;
+	private String endereco;
+	private String email;
+	private int telefone;
+	private double cpf;
+	private int id_cliente;
+	// para clientes sem id sera considerado valor maximo
+	
+public Cliente(String nome, String endereco, String email, double cpf, int telefone ){
+	this.nome= nome;
+	this.cpf = cpf;
+	this.email = email;
+	this.endereco = endereco;
+	this.telefone = telefone;
+	this.id_cliente = Integer.MAX_VALUE; }
+
+public String getNome() {
+	return nome;
+}
+
+public void setNome(String nome) {
+	this.nome = nome;
+}
+
+public String getEndereco() {
+	return endereco;
+}
+
+public void setEndereco(String endereco) {
+	this.endereco = endereco;
+}
+
+public String getEmail() {
+	return email;
+}
+
+public void setEmail(String email) {
+	this.email = email;
+}
+
+public double getCpf() {
+	return cpf;
+}
+
+public void setCpf(double cpf) {
+	this.cpf = cpf;
+}
+
+public int getId_cliente() {
+	return id_cliente;
+}
+
+public void setId_cliente(int id_cliente) {
+	this.id_cliente = id_cliente;
+}
+
+public int getTelefone() {
+	return telefone;
+}
+
+public void setTelefone(int telefone) {
+	this.telefone = telefone;
+}
+
+
+	
 }
