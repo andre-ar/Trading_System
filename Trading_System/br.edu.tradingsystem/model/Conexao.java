e08ee27aa5205863e142de7df49f24facf10b938
wemy <wemy@wemy-Inspiron-5423>
2014-08-26 17:18:42 -0300

    criado o properties e inicio da conexão com bundle

--- Trading_System/br.edu.tradingsystem/model/Conexao.java
+++ Trading_System/br.edu.tradingsystem/model/Conexao.java
@@ -2,40 +2,39 @@ package model;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
-
-import beans.Cliente;
+import java.util.ResourceBundle;
 
 public class Conexao {
 	Connection conn;
+	private static ResourceBundle config;
 	
-public Connection getConnection(){
-	
-	try {
-		Class.forName("conn.mysql.jdbc.Driver");
-		conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/trading", "root", "root");
-		System.out.println("conexao aceita");
-		return conn;
+	public Connection getConnection(){
 		
-	} catch (ClassNotFoundException | SQLException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
+		try {
+			Class.forName("conn.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/trading", "root", "root");
+			System.out.println("conexao aceita");
+			return conn;
+			
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("erro de conexao");
+		return null;	
 	}
-	System.out.println("erro de conexao");
-	return null;	
-}
 
-public void fechar_conexao(){
-	
-	try {
-		conn.close();
-	} catch (SQLException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
+	public void fechar_conexao(){
+		
+		try {
+			conn.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("conexao finalizada");
 	}
-	System.out.println("conexao finalizada");
-}
 
 
 }
